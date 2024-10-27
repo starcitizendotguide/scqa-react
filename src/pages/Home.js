c1486c4723c1a122a5429a5e73e90c3fda6855ab
jan <jan@krueger-jan.de>
2024-10-27 03:25:32 +0100

    fix toggle w/ algolia logo alignment

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -147,15 +147,11 @@ const App = ({ navigation, location }) => {
             }
 
             toggles={
-              <div className='mt-2'>
-                <div className='inline-block'>
                   <label className="inline-flex items-center cursor-pointer">
                     <input type="checkbox" checked={highlightQuery} onChange={event => setHighlightQuery(!highlightQuery)} className="sr-only peer" />
                     <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-yellow-400"></div>
                     <span className="ms-3 text-sm font-medium text-gray-200">Highlight Matches <i className="fas text-yellow-400 fa-highlighter"></i></span>
                   </label>
-                </div>
-              </div>
             }
           />
           <SearchResults highlightQuery={highlightQuery} />
